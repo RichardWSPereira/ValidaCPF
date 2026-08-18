78152ae171c04609993faf35925da1d71fb9e9be
RICHARD WILLIAM DA SILVA PEREIRA <srichardwilliam@hotmail.com>
2026-08-18 18:34:05 -0300

    Update de indentação organização de código

--- validaCPF.js
+++ validaCPF.js
@@ -18,8 +18,11 @@ class validaCPF {
     // Função que vai gerar um novo cpf com os 9 primeiros digitos do cpf enviado, mas os dois digitos que serão calculados por outra função de acorod com o calculo do governo de validação
     geraNovoCPF() {
         const cpfSemDigito = this.cpfLimpo.slice(0, -2); // Reduzido o cpf apenas para os primeiros 9 dígitos necessários para a conta, inicialmente
+        
         const digito1 = validaCPF.geraDigito(cpfSemDigito);
+
         const digito2 = validaCPF.geraDigito(cpfSemDigito + digito1); // A proxima chamada no metodo geraDigito acrescentamos o decimo digito encontrado
+
         this.novoCPF = cpfSemDigito + digito1 + digito2; // completamos o CPF a partir dos dois ultimos digitos que seriam conforme a regra de validação
 
     }
@@ -29,12 +32,15 @@ class validaCPF {
         const cpfArray = Array.from(cpfSemDigito); // transformando os 9 primeiros dígitos em um array para a manipulação dos dígitos para o cálculo
 
         let regressivo = cpfArray.length + 1; // Será usado o total de dígitos regressivamente 
+
         const total = cpfArray.reduce((ac, val) => {
             ac += (Number(val) * regressivo);
             regressivo--;
             return ac;
         }, 0); // Usa-se o reduce para criar um sequencia regressiva até a quantidade do array. Assim pode-se extrair o total da regressão multiplicada pelo respectivo elemento do array com os digitos do cpf
+
         const digito = 11 - (total % 11); // Este total será usado na regra de validação do decimo primeiro e segundo digito do CPF
+
         return digito > 9 ? '0' : String(digito); //Expressão ternaria para seguir a regra de validação, se o digito depois do calculo da regra for maior que 9, o proximo digito do CPF terá que ser zero.
     }
 
